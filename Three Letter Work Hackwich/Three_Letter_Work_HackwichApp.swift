fa89ec0a8d7a09dfbc1cf376f5c30f9ad916d66a
Briana-Koo <RoyalBlueSnow@gmail.com>
2021-10-27 10:18:07 -0500

    Initial Commit

---
+++ Three Letter Work Hackwich/Three_Letter_Work_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Three_Letter_Work_HackwichApp.swift
+//  Three Letter Work Hackwich
+//
+//  Created by Student on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct Three_Letter_Work_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
